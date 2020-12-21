3c5a116f3f9d887335613ad3cc40c70bf0eec018
JooYoo <jooyoo@outlook.com>
2020-12-21 13:47:41 +0100

    setup: pc test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>BigDay start</p>
+        <p>BigDay start 😀</p>
       </header>
     </div>
   );
