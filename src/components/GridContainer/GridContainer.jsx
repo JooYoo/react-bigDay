edb6a3222008d963b5996366e03c965f3ef50ab0
JooYoo <jooyoo@outlook.com>
2020-12-22 13:44:30 +0100

    dev: raw gridContainer and slideUp

---
+++ src/components/GridContainer/GridContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function GridContainer() {
+  return <div>GridContainer works</div>;
+}
+
+export default GridContainer;
---
+++ src/components/SlideUp/SlideUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SlideUp() {
+  return (
+    <div>
+      SlideUp works
+      <div>small title</div>
+    </div>
+  );
+}
+
+export default SlideUp;
