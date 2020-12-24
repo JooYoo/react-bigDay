a4f8e726e2b94b3cdab0e846e2a222446a7f2cbf
JooYoo <jooyoo@outlook.com>
2020-12-24 12:10:48 +0100

    dev: bigDayList retrive items

--- src/components/SlideUp/SlideUp.jsx
+++ src/components/SlideUp/SlideUp.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import BigDayList from './BigDayList/BigDayList';
 import slideUpStyle from './SlideUp.module.scss';
 
 function SlideUp() {
   return (
-    <div className={slideUpStyle['slide-up-container']}>SlideUp works</div>
+    <div className={slideUpStyle['slide-up-container']}>
+      <BigDayList/>
+    </div>
   );
 }
 
