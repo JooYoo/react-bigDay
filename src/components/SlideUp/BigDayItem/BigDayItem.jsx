71755cbf29a4d72f7e540ab3a4e237473a8b5a30
JooYoo <jooyoo@outlook.com>
2020-12-27 13:58:53 +0100

    dev: remove bigDay item

--- src/components/SlideUp/BigDayItem/BigDayItem.jsx
+++ src/components/SlideUp/BigDayItem/BigDayItem.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 function BigDayItem(props) {
-    return (
-        <div>
-           {props.id} | {props.bigDay.title} | {props.bigDay.begin} | {props.bigDay.end}
-        </div>
-    )
+  return (
+    <div>
+      {props.id} | {props.bigDay.title} | {props.bigDay.begin} |
+      {props.bigDay.end}
+      <button onClick={() => props.removeBigDay(props.id)}>X</button>
+    </div>
+  );
 }
 
-export default BigDayItem
+export default BigDayItem;
--- src/components/SlideUp/BigDayList/BigDayList.jsx
+++ src/components/SlideUp/BigDayList/BigDayList.jsx
@@ -1,52 +1,72 @@
-import React, {useState}  from 'react'
+import React, { useState } from 'react';
 import BigDayItem from '../BigDayItem/BigDayItem';
 import BigDayForm from '../BigDayForm/BigDayForm';
 
 function BigDayList() {
-    const initBigDayList = [
-        {
-            id: 0,
-            title: 'Zelda release',
-            begin: '2020/12/12',
-            end: '2021/05/01'
-        },
-        {
-            id: 1,
-            title: 'Borderlands release',
-            begin: '2020/12/12',
-            end: '2021/05/01'
-        },
-        {
-            id: 2,
-            title: 'Mario release',
-            begin: '2020/12/12',
-            end: '2021/05/01'
-        }
-    ]
+  const initBigDayList = [
+    {
+      id: 0,
+      title: 'Zelda release',
+      begin: '2020/12/12',
+      end: '2021/05/01',
+    },
+    {
+      id: 1,
+      title: 'Borderlands release',
+      begin: '2020/12/12',
+      end: '2021/05/01',
+    },
+    {
+      id: 2,
+      title: 'Mario release',
+      begin: '2020/12/12',
+      end: '2021/05/01',
+    },
+  ];
 
-    const [bigDayList, setbigDayList] = useState(initBigDayList);
+  const [bigDayList, setBigDayList] = useState(initBigDayList);
 
-    const addBigDay = (title, begin, end) =>{
-        const newBigDayList = [...bigDayList, {
-            title: title,
-            begin: begin,
-            end: end
-        }]
+  /* ----------------------------- add new BigDay ----------------------------- */
+  const addBigDay = (title, begin, end) => {
+    const newBigDayList = [
+      ...bigDayList,
+      {
+        title: title,
+        begin: begin,
+        end: end,
+      },
+    ];
 
-        setbigDayList(newBigDayList);
-    }
+    setBigDayList(newBigDayList);
+  };
 
-    let bigDays = bigDayList.map((bigDay, index) => (
-        <BigDayItem key={index} id={index} bigDay={bigDay}/>
-    ))
+  /* ------------------------------ remove BigDay ----------------------------- */
+  const removeBigDay = (index) => {
+    // clone the bigDayList
+    const newBigDayList = [...bigDayList];
+    // remove bigDayItem via index
+    newBigDayList.splice(index, 1);
+    // update the state
+    setBigDayList(newBigDayList);
+  };
 
-    return (
-        <div>
-            <BigDayForm addBigDay={addBigDay}/>
-            <hr/>
-            {bigDays}
-        </div>
-    )
+  /* ----------------------- iteration bigDayList items ----------------------- */
+  let bigDays = bigDayList.map((bigDay, index) => (
+    <BigDayItem
+      key={index}
+      id={index}
+      bigDay={bigDay}
+      removeBigDay={removeBigDay}
+    />
+  ));
+
+  return (
+    <div>
+      <BigDayForm addBigDay={addBigDay} />
+      <hr />
+      {bigDays}
+    </div>
+  );
 }
 
-export default BigDayList
+export default BigDayList;
