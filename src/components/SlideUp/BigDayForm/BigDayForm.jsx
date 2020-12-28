742126e1d8f63f8acbe70b14bb8d4d095a600d3e
JooYoo <jooyoo@outlook.com>
2020-12-28 13:20:28 +0100

    dev: setup react-dates

--- src/components/SlideUp/BigDayForm/BigDayForm.jsx
+++ src/components/SlideUp/BigDayForm/BigDayForm.jsx
@@ -1,31 +1,36 @@
 import React, { useState } from 'react';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import { DateRangePicker } from 'react-dates';
 
 function BigDayForm(props) {
+  const initDate = {
+    startDate: null,
+    endDate: null,
+  };
+
   const [title, setTitle] = useState('');
-  const [begin, setBegin] = useState('2020/01/01');
-  const [end, setEnd] = useState('2020/12/31');
+  const [{ startDate, endDate }, setDate] = useState(initDate);
+  const [focusedInput, setFocusedInput] = useState(null);
 
   const setTitleHandler = (e) => {
     setTitle(e.target.value);
   };
 
-  const setBeginHandler = (e) => {
-    setBegin(e.target.value);
-  };
-
-  const setEndHandler = (e) => {
-    setEnd(e.target.value);
-  };
-
   const submitFormHandler = (e) => {
     // add new BigDay
-    props.addBigDay(title, begin, end);
+    props.addBigDay(title, String(startDate._d), String(endDate._d));
 
     // after add new BigDay clean up textbox
-    setTitle('')
+    setTitle('');
 
     // avoid after submit refresh
     e.preventDefault();
+  };
+
+  const onDatesChangeHandler = ({ startDate, endDate }) => {
+    setDate({ startDate, endDate });
+    console.log(startDate._d);
   };
 
   return (
@@ -35,12 +40,18 @@ function BigDayForm(props) {
         <input type="text" value={title} onChange={(e) => setTitleHandler(e)} />
       </div>
       <div>
-        <label>begin:</label>
-        <input type="text" value={begin} onChange={(e) => setBeginHandler(e)} />
-      </div>
-      <div>
-        <label>end:</label>
-        <input type="text" value={end} onChange={(e) => setEndHandler(e)} />
+        <label> Date Range: </label>
+        <DateRangePicker
+          startDate={startDate} // momentPropTypes.momentObj or null,
+          startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+          endDate={endDate} // momentPropTypes.momentObj or null,
+          endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+          onDatesChange={({ startDate, endDate }) =>
+            onDatesChangeHandler({ startDate, endDate })
+          } // PropTypes.func.isRequired,
+          focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+          onFocusChange={(focusedInput) => setFocusedInput(focusedInput)} // PropTypes.func.isRequired,
+        />
       </div>
       <button type="submit">submit</button>
     </form>
