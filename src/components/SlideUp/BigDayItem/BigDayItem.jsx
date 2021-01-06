7817b604aa5acdf52e7ea88819a670b12a681b90
JooYoo <jooyoo@outlook.com>
2021-01-06 14:37:17 +0100

    dev: slideup toggle

--- src/components/SlideUp/BigDayItem/BigDayItem.jsx
+++ src/components/SlideUp/BigDayItem/BigDayItem.jsx
@@ -4,9 +4,8 @@ function BigDayItem(props) {
   return (
     <div>
       <button onClick={() => props.removeBigDay(props.id)}>X</button>
-      {props.id} | {props.bigDay.title} | {props.bigDay.begin} ~
-      {props.bigDay.end} | {props.bigDay.restDays} |
-      {props.bigDay.highlightColor}
+      {props.bigDay.title} | {props.bigDay.begin} ~{props.bigDay.end} |{' '}
+      {props.bigDay.restDays} |{props.bigDay.highlightColor}
     </div>
   );
 }
--- src/components/SlideUp/SlideUp.jsx
+++ src/components/SlideUp/SlideUp.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BigDayList from './BigDayList/BigDayList';
 import slideUpStyle from './SlideUp.module.scss';
 
 function SlideUp() {
+  const [isSlideUp, setIsSlideUp] = useState(false);
+
+  /* --------------------------- slide-up animation --------------------------- */
+  const onSlideUp = () => {
+    setIsSlideUp(!isSlideUp);
+  };
+
   return (
-    <div className={slideUpStyle['slide-up-container']}>
-      <BigDayList />
+    <div className={slideUpStyle['slideup-modal-host']}>
+      <div
+        id="slideup__container"
+        className={
+          slideUpStyle['slideup__container'] +
+          ' ' +
+          (isSlideUp
+            ? slideUpStyle['slide-up-anim']
+            : slideUpStyle['slide-down-anim'])
+        }
+      >
+        <button
+          id="slideup__btn"
+          className={slideUpStyle['slideup__btn']}
+          onClick={onSlideUp}
+        ></button>
+        <div className={slideUpStyle['slideup__scroll-panel']}>
+          <BigDayList />
+        </div>
+      </div>
     </div>
   );
 }
